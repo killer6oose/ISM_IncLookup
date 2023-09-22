e99d4a5938991e45da4357d1c27a7d0991b3ab4d
Andrew <38707579+killer6oose@users.noreply.github.com>
2023-09-22 16:47:25 -0400

    Update ISM_DB.cs

--- root/ISM_DB.cs
+++ root/ISM_DB.cs
@@ -1,17 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ISM_IncLookup
 {
     public class ISM_DB : DbContext
     {
-        public ISM_DB(DbContextOptions<ISM_DB> options)
+        private readonly IConfiguration _configuration;
+
+        public ISM_DB(DbContextOptions<ISM_DB> options, IConfiguration configuration)
             : base(options)
         {
+            _configuration = configuration;
         }
 
-        // Define your database tables here as DbSet properties
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Access the connection string from appsettings.json
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            
+            // Use the connection string to configure the database context
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        // Define the DbSet property for the Incident table
         public DbSet<Incident> Incidents { get; set; }
-        // Add more DbSet properties for other tables if needed
+
+        // No need to define DbSet properties for other tables if not needed
 
         // Configure database relationships, constraints, and more here
         protected override void OnModelCreating(ModelBuilder modelBuilder)
